51474038237275e3024718c652b74ebad30116fd
Scott Boms <scottboms@gmail.com>
2011-07-26 19:58:42 +0000

    Adding support for NSM Addon Updater.

---
+++ system/expressionengine/third_party/reelocate/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+	* @package		Reelocate
+	* @version		1.1
+	* @author			Adrian McNeil
+	* @copyright	Copyright (c) 2011 Adrian McNeil <http://exp-resso.com>
+	* @link				http://github.com/crescendo/reelocate
+	*/
+
+	$config['name'] = "Reelocate";
+	$config['version'] = "1.1";
+	$config['nsm_addon_updater']['versions_xml'] = "http://exp-resso.com/ee-versions/reelocate.xml";
